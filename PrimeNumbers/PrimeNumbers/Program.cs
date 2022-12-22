291cde1bf04176df98a8306cca158f44b084fbab
Sala004 <121264378+Sala004@users.noreply.github.com>
2022-12-23 00:20:39 +0200

    Add files via upload

---
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace PrimeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, i, j;
+
+            Console.Write("Enter Starting Number: ");    
+            
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Ending Number: ");       
+            n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nPrime Numbers between {0} and {1} are: ", n1, n2);
+
+            for (i = n1; i <= n2; i++)      
+            {
+                for (j = 2; j < i; j++)                    
+                {
+                    if (i % j == 0)
+                        break;
+                }               
+                if (j == i)                                                   
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
